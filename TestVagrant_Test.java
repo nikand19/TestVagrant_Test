7c65f5e2186d9fc6c910e10be287d2ea5dfcdf85
Nikhil <93384362+nikand19@users.noreply.github.com>
2023-11-18 13:17:05 +0530

    Update TestVagrant_Test.java

--- TestVagrant_Test.java
+++ TestVagrant_Test.java
@@ -1,42 +1,41 @@
 import java.util.*;
 
-class Shopping {
-    class Basket {
-        String Product;
-        float Unit_price;
-        float Gst;
-        float Quantity;
-        String ProductWithMaxGST;
-    }
-        Basket(String Product, int Unit_price, float Gst, float Quantity){
-            
-        }
-
-    public float FindGST(float Unit_price,int Quantity,float Gst){
-            float GST=0;
-            GST+=Unit_price*Quantity*Gst/100;
-            return GST;
-        
-        }
-
-    public float FindDiscount(float Unit_price,float Quantity){
-            float Discount=0;
-            Discount+=Unit_price*Quantity*5/100;
-            return Discount;
-        }
-
-    public float FindMaxGST (float unit_price, float Quantity ){
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<Product.length;i++){
-            if(FindGST>max){
-                max=FindGST;
+public class Bucket {
+    public String maximumGSTProduct(String[] product, int[] price, int[] gst, int[] quan) {
+        int maxGST = Integer.MIN_VALUE;
+        int maxIndex = -1;
+        for (int i = 0; i < product.length; i++) {
+            int currentMaxGST = price[i] * quan[i] * gst[i];
+            if (currentMaxGST > maxGST) {
+                maxGST = currentMaxGST;
+                maxIndex = i;
             }
         }
-        Amount_paid+=unit_price*Quantity+FindDiscount+FindGST;
-        return Amount_paid;
+        return product[maxIndex];
+    }
+
+    public int totalAmount(int[] price, int[] gst, int[] quan) {
+        int total = 0;
+        for (int i = 0; i < price.length; i++) {
+            int current;
+            if (price[i] > 500) {
+                current = ((price[i] * (100 + gst[i]) * quan[i]) / 100) - 0.05 * ((price[i] * (100 + gst[i]) * quan[i]) / 100);
+            } else {
+                current = (price[i] * (100 + gst[i]) * quan[i]) / 100;
+            }
+            total += current;
         }
+        return total;
+    }
 
     public static void main(String[] args) {
+        Example e = new Example();
+        String[] product = {"Leather Wallet", "Umbrella", "Cigarette", "Honey"};
+        int[] price = {1100, 900, 200, 100};
+        int[] gst = {18, 12, 28, 0};
+        int[] quan = {1, 4, 3, 2};
 
+        System.out.println("Maximum GST product is: " + e.maximumGSTProduct(product, price, gst, quan));
+        System.out.println("Total Amount Paid to Shopkeeper is: " + e.totalAmount(price, gst, quan));
     }
 }
